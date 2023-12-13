615df8e2dfa43686e5664bc390b717507c295531
alex-18 <115183782+humanbean1@users.noreply.github.com>
2023-12-13 14:02:22 -0500

    djf

--- src/main/java/edu/ics4u/alex/patron.java
+++ src/main/java/edu/ics4u/alex/patron.java
@@ -3,5 +3,14 @@ package edu.ics4u.alex;
 public class patron {
 
     public static void main(String[] args){
+        int b = 10;
+        int[] a = new int[b];
+        for (int i =0 ; i==b-1; i++){
+a[i]=i;
+        }
+        seqence c = new normal();
+         seqence d = new adapter();
+       System.out.println(c.Seqence(a)); 
+       System.out.println(d.Seqence(a));
 
 }}
